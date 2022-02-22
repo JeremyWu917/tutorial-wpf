cf57911c891515853a7e4455624880db1ad3421b
jeremy <1280442096@qq.com>
2022-02-22 10:48:50 +0800

    :coffee: wpf App and params sample

---
+++ src/tutorial-wpf/02_Wpf_App/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _02_Wpf_App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Create the startup window
+            MainWindow wnd = new();
+            // Do stuff here, e.g. to the window
+            // wnd.Title = "Something else";
+            if (e.Args.Length == 1)
+            {
+                _ = MessageBox.Show("Now opening file: \n\n" + e.Args[0]);
+            }
+            // Show the window
+            wnd.Show();
+        }
+    }
+}
